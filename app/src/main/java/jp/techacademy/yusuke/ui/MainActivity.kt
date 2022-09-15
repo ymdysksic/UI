8de32055582d60263a34e5ce9f1bd13931a813b0
ymdysk <ymdysk@sic.shimano.co.jp>
2022-09-15 16:19:00 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yusuke/ui/MainActivity.kt
@@ -0,0 +1,18 @@
+package jp.techacademy.yusuke.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log  // ← ここを追加
+import android.view.View  // ← ここを追加
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener {
+            Log.d("UI_PARTS", "ボタンをタップしました")
+        }
+    }
+}
